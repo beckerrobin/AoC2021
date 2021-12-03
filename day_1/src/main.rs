a190667029465949ca7ad462bd83d44536caed65
Robin Becker <robin.becker@afry.com>
2021-12-03 10:07:22 +0100

    day 1 part 2

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -3,6 +3,7 @@ use std::io::BufRead;
 use std::io::BufReader;
 
 fn main() {
+    // Arrange
     let file = fs::File::open("input.txt").expect("No such file");
     let buf = BufReader::new(file);
     let collection: Vec<i16> = buf
@@ -10,6 +11,7 @@ fn main() {
         .map(|l| l.expect("could not parse").parse::<i16>().unwrap())
         .collect();
 
+    // Part 1
     let mut counter = 0;
     for i in 1..collection.len() {
         if collection[i] > collection[i - 1] {
@@ -17,4 +19,13 @@ fn main() {
         }
     }
     println!("Part 1: {}", counter);
+    
+    // Part 2
+    counter = 0;
+    for i in 1..collection.len()-2 {
+        if collection[i-1..i+2].iter().sum::<i16>() < collection[i..i+3].iter().sum::<i16>() {
+            counter += 1;
+        }
+    }
+    println!("Part 2: {}", counter);
 }
