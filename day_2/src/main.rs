b6a7b57c3d2678493b1e9a9d389842f3656227b9
Robin Becker <robin.becker@afry.com>
2021-12-11 10:23:08 +0100

    day 2 part 2

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -7,15 +7,17 @@ fn main() {
     let file = fs::File::open("input.txt").expect("No such file");
     let buf = BufReader::new(file);
     let collection: Vec<String> = buf.lines().map(|l| l.expect("could not parse")).collect();
+    let mut commands: Vec<(&str, u32)> = Vec::new();
 
-    let mut horizontal:u32 = 0;
-    let mut depth:u32 = 0;
-
+    let mut horizontal: u32 = 0;
+    let mut depth: u32 = 0;
     // Part 1
     for i in 0..collection.len() {
         //println!("{}", collection[i].find(char::is_whitespace).unwrap());
-        let (command, n): (&str, &str) = collection[i].split_at(collection[i].find(char::is_whitespace).unwrap());
+        let (command, n): (&str, &str) =
+            collection[i].split_at(collection[i].find(char::is_whitespace).unwrap());
         let amount = n.trim_start().parse::<u32>().unwrap();
+        commands.push((command, amount));
         match command {
             "forward" => horizontal += amount,
             "up" => depth -= amount,
@@ -24,4 +26,22 @@ fn main() {
         }
     }
     println!("Part 1: {}", horizontal * depth);
+    
+    // Part 2
+    let mut aim = 0;
+    horizontal = 0;
+    depth = 0;
+    for i in 0..commands.len() {
+        let (command, amount) = commands[i];
+        match command {
+            "forward" => {
+                horizontal += amount;
+                depth += amount * aim;
+            }
+            "up" => aim -= amount,
+            "down" => aim += amount,
+            _ => panic!(),
+        }
+    }
+    println!("Part 2: {}", horizontal * depth);
 }
